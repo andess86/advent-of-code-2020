0182fec36b948833dfd11fd9ad1995514f70a987
Anders Svensson <andess86@gmail.com>
2020-12-02 13:50:19 +0100

    Add day 2, part 2

--- 02/solution.js
+++ 02/solution.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf-8', (input, err) => {
   if (err) {
@@ -21,18 +20,35 @@ const massageData = (passwords) =>
     return data;
   });
 
-const isPasswordsValid = (passwords) => {
+const isPasswordsValid = (passwords, newPolicy = false) => {
   let numberOfValidPasswords = 0;
   passwords.map((password) => {
     //  match function doesn't accept string literals as regex patterns,
     // need the constructor of the RegExp object and pass that to the String.match function:
     var regEx = new RegExp(password[2], 'g');
-    var count = (password[3].match(regEx) || []).length;
-    if (count >= password[0] && count <= password[1]) {
-      numberOfValidPasswords++;
+
+    if (!newPolicy) {
+      var count = (password[3].match(regEx) || []).length;
+      if (count >= password[0] && count <= password[1]) {
+        numberOfValidPasswords++;
+      }
+    }
+    if (newPolicy) {
+      let matches = 0;
+      while ((match = regEx.exec(password[3])) != null) {
+        if (match.index + 1 === password[0]) {
+          matches++;
+        }
+        if (match.index + 1 === password[1]) {
+          matches++;
+        }
+      }
+      if (matches === 1) {
+        numberOfValidPasswords++;
+      }
     }
   });
-return numberOfValidPasswords;
+  return numberOfValidPasswords;
 };
 
 const solve1 = () => {
@@ -41,4 +57,11 @@ const solve1 = () => {
   console.log(correctPasswords + ' passwords are valid.');
 };
 
+const solve2 = () => {
+  const data = massageData(passwords);
+  let correctPasswords = isPasswordsValid(data, 'newPolicy');
+  console.log(correctPasswords + ' passwords are valid.');
+};
+
 solve1();
+solve2();
