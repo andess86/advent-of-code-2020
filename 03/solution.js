5749ee115ce680e7be146b025f618a7f4af14fe5
Anders Svensson <andess86@gmail.com>
2020-12-03 17:43:08 +0100

    Add day 3, part 2

--- 03/solution.js
+++ 03/solution.js
@@ -17,9 +17,9 @@ const makeDataTable = (data) => {
   return arr;
 };
 
-const moveToboggan = (currentLocation) => {
-  currentLocation.lat = currentLocation.lat + 3;
-  currentLocation.long = currentLocation.long + 1;
+const moveToboggan = (currentLocation, slope) => {
+  currentLocation.lat = currentLocation.lat + slope.dx;
+  currentLocation.long = currentLocation.long + slope.dy;
   if (currentLocation.lat >= rightSideOfMap) {
     switch (currentLocation.lat) {
       case rightSideOfMap:
@@ -31,6 +31,19 @@ const moveToboggan = (currentLocation) => {
       case rightSideOfMap + 2:
         currentLocation.lat = 2;
         break;
+      case rightSideOfMap + 3:
+        currentLocation.lat = 3;
+        break;
+      case rightSideOfMap + 4:
+        currentLocation.lat = 4;
+        break;
+      case rightSideOfMap + 5:
+        currentLocation.lat = 5;
+        break;
+      case rightSideOfMap + 6:
+        currentLocation.lat = 6;
+        break;
+
       default:
         break;
     }
@@ -38,26 +51,52 @@ const moveToboggan = (currentLocation) => {
   return currentLocation;
 };
 
-const traverseWorld = (datatable) => {
+const traverseWorld = (worldMap, slope = { dx: 3, dy: 1 }) => {
   let characters = [];
   let currentLocation = {
     lat: 0,
     long: 0,
   };
   while (currentLocation.long <= bottomOfMap) {
-    currentLocation = moveToboggan(currentLocation);
+    currentLocation = moveToboggan(currentLocation, slope);
     if (currentLocation.long < bottomOfMap) {
-      characters.push(datatable[currentLocation.long][currentLocation.lat]);
+      characters.push(worldMap[currentLocation.long][currentLocation.lat]);
     }
   }
   return characters;
 };
 
 const solve1 = () => {
-  const dataTable = makeDataTable(map);
-  const terrainTypes = traverseWorld(dataTable);
+  const worldMap = makeDataTable(map);
+  const terrainTypes = traverseWorld(worldMap);
   var numberOfTrees = terrainTypes.filter((item) => item === '#').length;
   console.log(numberOfTrees + ' trees were encountered');
 };
 
+solve2 = () => {
+  const worldMap = makeDataTable(map);
+  const slopes = [
+    { dx: 1, dy: 1 },
+    { dx: 3, dy: 1 },
+    { dx: 5, dy: 1 },
+    { dx: 7, dy: 1 },
+    { dx: 1, dy: 2 },
+  ];
+  const treesPerSlope = [];
+
+  slopes.map((slope) => {
+    const terrainTypes = traverseWorld(worldMap, slope);
+    var numberOfTrees = terrainTypes.filter((item) => item === '#').length;
+    treesPerSlope.push(numberOfTrees);
+  });
+  const totalTrees = treesPerSlope.reduce(
+    (product, trees) => product * trees,
+    1
+  );
+  console.log(
+    totalTrees + ' is the number if multiplying trees from all slopes.'
+  );
+};
+
 solve1();
+solve2();
